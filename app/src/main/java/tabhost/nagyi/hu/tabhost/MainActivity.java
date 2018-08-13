3c2b446b88cf20b019547eea945feec0ffcc877d
P1stiRS <pistirs96@gmail.com>
2018-08-13 16:24:47 +0200

    tested version

--- app/src/main/java/tabhost/nagyi/hu/tabhost/MainActivity.java
+++ app/src/main/java/tabhost/nagyi/hu/tabhost/MainActivity.java
@@ -1,9 +1,7 @@
 package tabhost.nagyi.hu.tabhost;
 
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TabHost;
 
 
